31a6512829760adacaa5464349d717089827c037
mwarne97 <51534674+mwarne97@users.noreply.github.com>
2022-01-18 12:50:30 -0400

    Update processor.cpp

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,12 @@
 #include "processor.h"
 
+//Added In
+#include "linux_parser.h"
+
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization() { 
+  long jiffies_active = LinuxParser::ActiveJiffies();
+  long jiffies_total = LinuxParser::Jiffies();
+  util_ = static_cast<float>(jiffies_active) / static_cast<float>(jiffies_total);
+  return util_; 
+}
