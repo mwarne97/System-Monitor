8ed5e5fe84f382bed7dfd0fc6054c8041aa8da33
mwarne97 <51534674+mwarne97@users.noreply.github.com>
2022-01-19 13:09:08 -0400

    Update process.cpp

--- src/process.cpp
+++ src/process.cpp
@@ -6,7 +6,6 @@
 
 #include "process.h"
 
-//Added
 #include "linux_parser.h"
 
 using std::string;
@@ -15,12 +14,10 @@ using std::vector;
 
 Process::Process(int pid) : pid_(pid) {}
 
-// TODO: Return this process's ID
 int Process::Pid() {
   return pid_; 
 }
 
-// TODO: Return this process's CPU utilization
 float Process::CpuUtilization() { 
   long hertz = sysconf(_SC_CLK_TCK);
   
@@ -28,30 +25,26 @@ float Process::CpuUtilization() {
   
   long seconds = Process::UpTime();
   
-  cpu_utilization_ = (total_time_seconds / seconds);
+  cpu_utilization_ = (static_cast<float>(total_time_seconds) / static_cast<float>(seconds));
   
   return cpu_utilization_;
 }
 
-// TODO: Return the command that generated this process
 string Process::Command() { 
   command_ = LinuxParser::Command(Pid());
   return command_; 
 }
 
-// TODO: Return this process's memory utilization
 string Process::Ram() { 
   ram_ = LinuxParser::Ram(Pid());
   return ram_; 
 }
 
-// TODO: Return the user (name) that generated this process
 string Process::User() { 
   user_ = LinuxParser::User(Pid());
   return user_;
 }
 
-// TODO: Return the age of this process (in seconds)
 long int Process::UpTime() { 
   long uptime_system_seconds = LinuxParser::UpTime();
   long uptime_process_ticks = LinuxParser::UpTime(Pid());
@@ -62,8 +55,6 @@ long int Process::UpTime() {
   return uptime_; 
 }
 
-// TODO: Overload the "less than" comparison operator for Process objects
-// REMOVE: [[maybe_unused]] once you define the function
 bool Process::operator<(Process const& a) const { 
   return std::stol(ram_) < std::stol(a.ram_);
 }
